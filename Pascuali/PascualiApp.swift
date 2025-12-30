4231f4d55649e293108a2336f92b9ede9cf9895a
ALEX DOVEN <myalexverse@gmail.com>
2025-12-30 01:25:48 -0600

    Initial Commit

---
+++ Pascuali/PascualiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PascualiApp.swift
+//  Pascuali
+//
+//  Created by Alex Tovar on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PascualiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
